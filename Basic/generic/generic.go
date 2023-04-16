ac48b67bdf51e327dd4e1ea6f5cfee8c98943e9f
ritiktyagi1 <ritiktyagi110@gmail.com>
2023-04-16 19:30:52 +0530

    added generic and maps file

---
+++ Basic/generic/generic.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+var pl = fmt.Println
+
+type MyConstraint interface {
+	int | float64
+}
+
+func getSumGen[T MyConstraint](x T, y T) T {
+	return x + y
+}
+func main() {
+	pl("5 + 4 =", getSumGen(5, 4))
+	pl("5.6 + 4.7 =", getSumGen(5.6, 4.7))
+}
